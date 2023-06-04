931b75542ec6631f502f7bde7290c2186bac7a70
gaytekin <gaytekin@anadolubank.com.tr>
2023-06-04 18:59:49 +0300

    write test

--- onlinecourse/tests.py
+++ onlinecourse/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
+from onlinecourse.models import Course
+
 # Create your tests here.
+class QuestionModelTests(TestCase):
+    def test_was_course_has_min_one(self):
+        courses = Course.objects.order_by('-total_enrollment')
+        self.assertGreaterEqual(courses.count(), 0)
